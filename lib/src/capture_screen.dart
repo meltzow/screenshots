89660e2033303ea8f40f9a4c93438f47150705de
Felix Gabler <felix.gabler98@gmail.com>
2021-08-30 13:53:11 +0100

    #1021 prepare move to integration_test
    Needs restructuring around languages

--- lib/src/capture_screen.dart
+++ lib/src/capture_screen.dart
@@ -1,11 +1,14 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+
 import 'config.dart';
 import 'globals.dart';
 
 /// Called by integration test to capture images.
-Future screenshot(final driver, Config config, String name,
+Future screenshotDriver(final driver, Config config, String name,
     {Duration timeout = const Duration(seconds: 30),
     bool silent = false,
     bool waitUntilNoTransientCallbacks = true}) async {
@@ -17,11 +20,21 @@ Future screenshot(final driver, Config config, String name,
 
     final pixels = await driver.screenshot();
     final testDir = '${config.stagingDir}/$kTestScreenshotsDir';
-    final file =
-        await File('$testDir/$name.$kImageExtension').create(recursive: true);
+    final file = await File('$testDir/$name.$kImageExtension').create(recursive: true);
     await file.writeAsBytes(pixels);
     if (!silent) print('Screenshot $name created');
   } else {
     if (!silent) print('Warning: screenshot $name not created');
   }
+}
+
+Future screenshot(IntegrationTestWidgetsFlutterBinding binding, WidgetTester tester, String name,
+    {bool silent = false}) async {
+  // This is required prior to taking the screenshot (Android only).
+  await binding.convertFlutterSurfaceToImage();
+
+  // Trigger a frame.
+  await tester.pumpAndSettle();
+  await binding.takeScreenshot(name);
+  if (!silent) print('Screenshot $name created');
 }
